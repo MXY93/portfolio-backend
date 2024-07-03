ffba57b62bba97a5e55109961781519a0bd31eab
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-07-03 22:53:19 +0200

    Port on 4000

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`);
