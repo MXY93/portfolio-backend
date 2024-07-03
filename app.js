065a6082d80f33ec46d06515908656a5ab704192
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-07-03 23:11:25 +0200

    error adjusted

--- app.js
+++ app.js
@@ -58,12 +58,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong');
 });
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
 
 
